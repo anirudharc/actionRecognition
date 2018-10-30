ca30c5e28aa3b498f9bcd691ae4eb49aca930e97
anirudharc <rcanirudha@gmail.com>
2018-10-30 02:09:51 -0400

    updated potion transforation

--- potion_transform.py
+++ potion_transform.py
@@ -5,20 +5,35 @@ import numpy as np
 def load_images_from_folder(path):
     images = []
 
-    for filename in os.listdir(path):
+    for filename in sorted(os.listdir(path)):
         img = cv2.imread(os.path.join(path,filename), cv2.IMREAD_GRAYSCALE)
-        # img = cv2.threshold(img, 1, 255, cv2.THRESH_TRUNC)
 
         if img is not None:
             potion_t = img.copy()
             potion_t[np.where(potion_t > [0])] = [255]
             potion_t = cv2.cvtColor(potion_t, cv2.COLOR_GRAY2BGR)
-            potion_t[np.where((potion_t == [255, 255, 255]).all(axis=2))] = [255, 0, 0]
 
             images.append(potion_t)
 
+    potion = []
     agg_image = images[0]
-    for idx, img in enumerate(images[1:]):
+    agg_image[np.where(True)] = [0, 0, 0]
+    canvas_split = len(images)//2 + 1
+    r = g = True
+    b = False
+
+    for idx, img in enumerate(images):
+
+        ratio = (idx % canvas_split)/canvas_split
+        if idx >= canvas_split:
+            r = False
+            b = True
+            ratio = 1 - ratio
+    
+        print(idx, idx % canvas_split, ratio)
+        img[np.where((img == [255, 255, 255]).all(axis=2))] = [b*(1 - ratio)*255, g*ratio*255, r*(1-ratio)*255]
+        potion.append(img)
+
         agg_image = cv2.add(agg_image, img)
         name = "/media/bighdd1/arayasam/actionRecognition/output/agg_image_" + str(idx) + ".png" 
         cv2.imwrite(name, img)
