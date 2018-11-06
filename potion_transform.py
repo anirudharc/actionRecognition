51fd095ccc626f02c1821ab55514a063dfee280d
anirudharc <rcanirudha@gmail.com>
2018-11-06 03:12:36 -0500

    update potion network

--- potion_transform.py
+++ potion_transform.py
@@ -2,12 +2,13 @@ import cv2
 import os
 import numpy as np
 
-def load_images_from_folder(path):
+def potion_transform(path):
     images = []
 
     for filename in sorted(os.listdir(path)):
         img = cv2.imread(os.path.join(path,filename), cv2.IMREAD_GRAYSCALE)
 
+        # Transform image to monochrome
         if img is not None:
             potion_t = img.copy()
             potion_t[np.where(potion_t > [0])] = [255]
@@ -15,7 +16,7 @@ def load_images_from_folder(path):
 
             images.append(potion_t)
 
-    potion = []
+    pose_list = []
     agg_image = images[0]
     agg_image[np.where(True)] = [0, 0, 0]
     canvas_split = len(images)//2 + 1
@@ -24,22 +25,23 @@ def load_images_from_folder(path):
 
     for idx, img in enumerate(images):
 
+        # Modulate ratio to get blend of red-green and green-blue
         ratio = (idx % canvas_split)/canvas_split
         if idx >= canvas_split:
             r = False
             b = True
             ratio = 1 - ratio
-    
-        # print(idx, idx % canvas_split, ratio)
+
+        # Normalize the pixel intensities - no. of frames
         # img[np.where((img == [255, 255, 255]).all(axis=2))] = [b*(1 - ratio)*255/canvas_split, g*ratio*255/canvas_split, r*(1-ratio)*255/canvas_split]
         img[np.where((img == [255, 255, 255]).all(axis=2))] = [b*(1 - ratio)*255, g*ratio*255, r*(1-ratio)*255]
-        potion.append(img)
+        pose_list.append(img)
 
         agg_image = cv2.add(agg_image, img)
         name = "/media/bighdd1/arayasam/actionRecognition/output/agg_image_" + str(idx) + ".png" 
         cv2.imwrite(name, img)
 
-    return images, agg_image
+    return images, pose_list, agg_image
 
-im_list, agg_image = load_images_from_folder("/media/bighdd1/arayasam/pose_model/output_heatmaps_folder/")
+im_list, pose_list, agg_image = potion_transform("/media/bighdd1/arayasam/dataset/output_heatmaps_folder/v_ApplyEyeMakeup_g08_c02/")
 cv2.imwrite("/media/bighdd1/arayasam/actionRecognition/output/agg_image.png", agg_image)
